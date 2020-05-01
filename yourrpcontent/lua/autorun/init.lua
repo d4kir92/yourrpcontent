cfbd6239f7978b65254a08cdea219f11fdc25fbd
Arno Zura <d4kir92@gmail.com>
2020-05-01 09:19:04 +0200

    AutoUpdate

--- yourrpcontent/lua/autorun/init.lua
+++ yourrpcontent/lua/autorun/init.lua
@@ -5,7 +5,7 @@ if SERVER then
 		yrp_content.name = "YourRP Content"
 		yrp_content.author = "D4KiR"
 		yrp_content.description = "Materials (Icons, ...)\nModels\nResources (Fonts, Translations, ...)"
-		yrp_content.icon = "https://steamuserimages-a.akamaihd.net/ugc/958587215620373887/A0EE118CDAF5D53E250005252377C48EADB11E75/"
+		yrp_content.icon = "https://imgur.com/fTj5MzK"
 		yrp_content.workshopid = "1189643820"
 		yrp_content.discord = "https://discord.gg/ZzbezAJ"
 		yrp_content.settings = ""
